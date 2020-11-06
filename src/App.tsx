e9ce1134b0427827901b471307582941e22cef40
Colin Zuo <colinzuo@qq.com>
2020-11-06 15:26:24 +0800

    add antd
    1. size increases from 158 KB to 840 KB before gzip after use DatePicker
    following the antd example

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import store from './lib/redux';
+import { ConfigProvider, DatePicker, message } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment, { Moment } from 'moment';
+import 'moment/locale/zh-cn';
 
+import store from './lib/redux';
 import InboxScreen from './components/InboxScreen';
 
-import './index.css';
+import './App.css';
+
+moment.locale('zh-cn');
+
 function App() {
+  const [date, setDate] = useState<Moment | null>(null);
+  const handleChange = (value: Moment | null) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`
+    );
+    setDate(value);
+  };
+
   return (
-    <Provider store={store}>
-      <InboxScreen />
-    </Provider>
+    <ConfigProvider locale={zhCN}>
+      <Provider store={store}>
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <DatePicker onChange={handleChange} />
+          <div style={{ marginTop: 16 }}>
+            当前日期：{date ? date!.format('YYYY年MM月DD日') : '未选择'}
+          </div>
+        </div>
+
+        <InboxScreen />
+      </Provider>
+    </ConfigProvider>
   );
 }
 export default App;
